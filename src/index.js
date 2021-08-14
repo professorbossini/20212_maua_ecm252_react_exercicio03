189665d54b7b0d00e2dcae3823e53aab2a7ca8c9
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-14 12:56:28 -0300

    Terminamos o exercicio 03

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+import doc1 from './images/doc1.jpg'
+
+const App = () => {
+
+    const docNames = {doc1: 'José da Silva', doc2: 'Maria da Silva', doc3: 'Jaquelines Mendes'}
+
+    const estilosComponentePrincipal = () => {
+        return {
+            width: 1280,
+            margin: 'auto',
+            border: '1px solid black',
+            backgroundColor: '#EEE',
+            borderRadius: 8,
+            padding: 12,
+            textAlign: 'center'
+        }
+    }
+
+    return (
+        
+        <div style={estilosComponentePrincipal()}>
+            <h2>Profissionais de saúde</h2>
+            <div style={{margin: 8, border: '1px solid #DDD', borderRadius: 8, padding: 8, display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+               
+                <div className="profissional">
+                    <p>{docNames.doc1}</p>
+                    <img src={doc1}/>
+                </div>
+
+                <div className="profissional">
+                    <p>{docNames.doc2}</p>
+                    <img src={process.env.PUBLIC_URL + 'doc2.jpg'}/>
+                </div>
+
+                <div className="profissional">
+                    <p>{docNames.doc3}</p>
+                    <img src="https://images.unsplash.com/photo-1613918108466-292b78a8ef95?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80" />
+                </div>
+            </div>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
